2fc2c5e41cdb7c22fad96e4dd1e0a2ba175a4ea5
VitaoNasser <victor.nasser@sga.pucminas.br>
2021-09-28 15:24:20 -0300

    First commit - update SceneDelegate

---
+++ PokemonMVC/Controller/PokedexController.swift
@@ -0,0 +1,24 @@
+//
+//  PokedexController.swift
+//  PokemonMVC
+//
+//  Created by Rodrigo Dias on 28/09/21.
+//
+
+import UIKit
+
+class PokedexController: UICollectionViewController {
+
+// MARK: - Properties
+
+// MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .blue
+    }
+
+}
+
+
