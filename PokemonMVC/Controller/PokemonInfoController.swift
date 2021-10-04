b8d7d70fa6e4a589dcbfd726b702fc206b407a8e
VitaoNasser <victor.nasser@sga.pucminas.br>
2021-10-04 12:29:12 -0300

    Add PokemonInfoController

---
+++ PokemonMVC/Controller/PokemonInfoController.swift
@@ -0,0 +1,27 @@
+//
+//  PokemonInfoController.swift
+//  PokemonMVC
+//
+//  Created by Rodrigo Dias on 01/10/21.
+//
+
+import UIKit
+
+class PokemonInfoController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewComponents()
+    }
+    
+    // MARK: - Helper Functions
+    
+    func configureViewComponents() {
+        view.backgroundColor = .white
+    }
+}
