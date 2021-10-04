af85150c3cfeaad3ed9bf211b3dc95f9a1b4b4e6
VitaoNasser <victor.nasser@sga.pucminas.br>
2021-10-04 13:00:03 -0300

    Add image and description on PokemonInfoController

--- PokemonMVC/Controller/PokemonInfoController.swift
+++ PokemonMVC/Controller/PokemonInfoController.swift
@@ -14,8 +14,23 @@ class PokemonInfoController: UIViewController {
     var pokemon: Pokemon? {
         didSet {
             navigationItem.title = pokemon?.name?.capitalized
+            imageView.image = pokemon?.image
+            infoLabel.text = pokemon?.description
         }
     }
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        return label
+    }()
     
     
     // MARK: - Init
@@ -29,5 +44,14 @@ class PokemonInfoController: UIViewController {
     
     func configureViewComponents() {
         view.backgroundColor = .white
+        
+        view.addSubview(imageView)
+        imageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 44, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 100, height: 100)
+        
+        view.addSubview(infoLabel)
+        infoLabel.anchor(top: nil, left: imageView.rightAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 44, paddingLeft: 16, paddingBottom: 0, paddingRight: 4, width: 0, height: 0)
+        infoLabel.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+        
+        
     }
 }
